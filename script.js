20a99c0411c4c74b8ee2138b9a5f1e32f94e03de
Roman Khlebnikov <39444417+khlebnikov-roman@users.noreply.github.com>
2019-08-23 18:31:43 +0300

    Update js

--- script.js
+++ script.js
@@ -1,25 +1,19 @@
 const createCalendar = (id, year, month) => {
     let elem = document.getElementById(id);
     let today = new Date();
-    console.log(today);
     let mon = today.getMonth() + 1;
-    console.log(mon);
     let y = today.getFullYear();
     const defaultTable = '<div class="table"><div>пн</div><div>вт</div><div>ср</div><div>чт</div><div>пт</div><div>сб</div><div>вс</div>';
     let table = defaultTable;
     
     // Заполнить первый ряд пустыми ячейками от пн
-
     const getDays = (month, year) => {
         return new Date(year, month, 0).getDate();
     };
-    console.log(getDays(mon, year));
-
 
     const definitionFirstDay = (y, mon) => {
         return new Date(y, mon - 1, 1);
     };
-    
     
     const addEmptySquare = (y, mon) => {
         let day = definitionFirstDay(y, mon).getDay();
@@ -36,55 +30,48 @@ const createCalendar = (id, year, month) => {
         elem.innerHTML = table;
     }
     
-    // Выводим текушщий месяц через select
-    const getMonth = () => {
+    // Выводим текушщий месяц и год через select
+    const getMonthAndYear = () => {
         let selectM = document.querySelector('.month');
-        selectM.value = month; 
+        selectM.value = month;
  
         selectM.addEventListener('change', () => {
             let currentMonth = selectM.selectedIndex + 1;
+            let currentYear = selectY.value;
             table = defaultTable;
-            definitionFirstDay(y, currentMonth);
-            console.log(definitionFirstDay(y, currentMonth));
-            addEmptySquare(y, currentMonth);
+            definitionFirstDay(currentYear, currentMonth);
+            addEmptySquare(currentYear, currentMonth);
 
-            for (let i = 1; i <= getDays(currentMonth, year); i++) {
+            for (let i = 1; i <= getDays(currentMonth, currentYear); i++) {
                 table += '<div class="day" data-id="">' + i + '</div>';
                 elem.innerHTML = table;
             }
         })
-    };
-    getMonth();
 
-    // Выводим текущий год в select
-    const getYear = () => {
         let max = y,
         selectY = document.querySelector('.year');
-       
+
         for (let i = y - 100; i <= max; i++) {
             let opt = document.createElement('option');
             opt.value = opt.innerHTML = i;
             selectY.appendChild(opt);
             selectY.value = year;
         }
-        
 
         selectY.addEventListener('change', () => {
+            let currentMonth = selectM.selectedIndex + 1;
             let currentYear = selectY.value;
             table = defaultTable;
-            definitionFirstDay(currentYear, mon);
-            console.log(definitionFirstDay(currentYear, mon));
-            addEmptySquare(currentYear, mon);
+            definitionFirstDay(currentYear, currentMonth);
+            addEmptySquare(currentYear, currentMonth);
 
-            for (let i = 1; i < getDays(mon, currentYear); i++) {
+            for (let i = 1; i <= getDays(currentMonth, currentYear); i++) {
                 table += '<div class="day" data-id="">' + i + '</div>';
                 elem.innerHTML = table;
             }
         })
     };
-    
-    getYear();
-
+    getMonthAndYear();
 }
 
 createCalendar('calendar', 2019, 8);
